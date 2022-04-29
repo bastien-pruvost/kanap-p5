66870cbb44ff35fc655641c1067e5e7251345739
Bastien Pruvost <bastien.mory@gmail.com>
2022-04-29 12:11:58 +0200

    Add config.js for dynamic Api URL

---
+++ front/js/config.js
@@ -0,0 +1,6 @@
+let apiUrl =
+  location.hostname === 'localhost' || location.hostname === '127.0.0.1'
+    ? 'http://localhost:3000'
+    : 'https://kanap-backend.pruvostbastien.fr';
+
+export { apiUrl };
